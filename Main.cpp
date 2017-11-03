eaf4dabe90b5f86bcafa76cda701c2d62fcfe83f
HongqiangWei <30711558+HongqiangWei@users.noreply.github.com>
2017-11-03 11:41:56 +0800

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,78 @@
+#include "stdafx.h"
+
+//--------------------------------------------------------------------
+//
+//	Perlin Noise Terrain Generator for modern C++ 11
+//
+//	The basic function writed by Ryo Suzuki <reputeless@gmail.com>
+//  
+//  The PNDEM() function writed by Hongqiang Wei <cwnuweihongqiang@126.com>
+//
+//  We use GDAL library to write the DEM file, the version of GDAL library is 1.9.1
+//
+//	For the license information refer to PerlinNoise.hpp.
+//
+//--------------------------------------------------------------------
+
+# include <iostream>
+# include <fstream>
+# include <sstream>
+# include "PerlinNoise.h"
+# include "dem.h"
+# include "Node.h"
+# include "utils.h"
+# include <string>
+
+template <class Type>
+inline constexpr const Type& Clamp(const Type& x, const Type& min, const Type& max)
+{
+	return (x < min) ? min : ((max < x) ? max : x);
+}
+
+int PNDEM(const int height, const int width, const double frequency, const int octaves, const std::uint32_t seed, char* outputfilepath);
+int main(int argc, char* argv[])
+{
+	argc = 2;
+	argv[1] = "F:\\TestData\\DEM Data\\Source File\\SizeBig\\PNDEM.tif";
+	std::string path(argv[1]);
+
+	std::cout << "---------------------------------\n";
+	std::cout << "* height    [1 .. 1000 ..50000]  \n";
+	std::cout << "* width     [1 .. 1000 ..50000]  \n";
+	std::cout << "* frequency [0.1 .. 8.0 .. 64.0] \n";
+	std::cout << "* octaves   [1 .. 8 .. 16]       \n";
+	std::cout << "* seed      [0 .. 2^32-1]        \n";
+	std::cout << "---------------------------------\n";
+
+	int height;
+	std::cout << "int height = ";
+	std::cin >> height;
+	height = Clamp(height, 1, 50000);
+
+	int width;
+	std::cout << "int width = ";
+	std::cin >> width;
+	width = Clamp(width, 1, 50000);
+
+	double frequency;
+	std::cout << "double frequency = ";
+	std::cin >> frequency;
+	frequency = Clamp(frequency, 0.1, 64.0);
+
+	int octaves;
+	std::cout << "int32 octaves    = ";
+	std::cin >> octaves;
+	octaves = Clamp(octaves, 1, 16);
+
+	std::uint32_t seed;
+	std::cout << "uint32 seed      = ";
+	std::cin >> seed;
+
+	if (PNDEM(height, width, frequency, octaves, seed, &path[0]) == 1) 
+		std::cout << "The new image is generated successfully!" << std::endl;
+	else
+		std::cout << "New image generation failed!" << std::endl;
+	return 1;
+}
+
+
---
+++ PNDEM.cpp
@@ -0,0 +1,84 @@
+#include "stdafx.h"
+# include <iostream>
+# include <fstream>
+# include <sstream>
+# include "PerlinNoise.h"
+# include "dem.h"
+# include "Node.h"
+# include "utils.h"
+# include <string>
+
+void calculateStatistics(const CDEM& dem, double* min, double* max, double* mean, double* stdDev)
+{
+	int width = dem.Get_NX();
+	int height = dem.Get_NY();
+
+	int validElements = 0;
+	double minValue, maxValue;
+	double sum = 0.0;
+	double sumSqurVal = 0.0;
+	for (int row = 0; row < height; row++)
+	{
+		for (int col = 0; col < width; col++)
+		{
+			if (!dem.is_NoData(row, col))
+			{
+				double value = dem.asFloat(row, col);
+
+				if (validElements == 0)
+				{
+					minValue = maxValue = value;
+				}
+				validElements++;
+				if (minValue > value)
+				{
+					minValue = value;
+				}
+				if (maxValue < value)
+				{
+					maxValue = value;
+				}
+
+				sum += value;
+				sumSqurVal += (value * value);
+			}
+		}
+	}
+
+	double meanValue = sum / validElements;
+	double stdDevValue = sqrt((sumSqurVal / validElements) - (meanValue * meanValue));
+	*min = minValue;
+	*max = maxValue;
+	*mean = meanValue;
+	*stdDev = stdDevValue;
+}
+
+int PNDEM(const int height, const int width, const double frequency, const int octaves, const std::uint32_t seed, char* outputfilepath) {
+	CDEM dem;
+	double geoTransformArgs[6];
+	geoTransformArgs[0] = 244730; geoTransformArgs[1] = 3; geoTransformArgs[2] = 0;
+	geoTransformArgs[3] = 5113198;geoTransformArgs[4] = 0; geoTransformArgs[5] = -3;
+
+	dem.SetHeight(height);
+	dem.SetWidth(width);
+	if (!dem.Allocate()) {
+		std::cout << "Failed to allocate memory correctly!" << std::endl;
+		return 0;
+	}
+
+	const PN::PerlinNoise perlin(seed);
+	const double fx = width / frequency;
+	const double fy = height / frequency;
+	for (int y = 0; y < height; ++y){
+		for (int x = 0; x < width; ++x){
+			dem.Set_Value(x, y, perlin.octaveNoise0_1(x / fx, y / fy, octaves) * 1000);
+		}
+	}
+
+	double min, max, mean, stdDev;
+	calculateStatistics(dem, &min, &max, &mean, &stdDev);
+	CreateGeoTIFF(outputfilepath, dem.Get_NY(), dem.Get_NX(),
+		(void *)dem.getDEMdata(), GDALDataType::GDT_Float32, geoTransformArgs,
+		&min, &max, &mean, &stdDev, -9999);
+	return 1;
+}
